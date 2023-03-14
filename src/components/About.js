81ef007b73abfad82a2a442a81b25151db2ac9b1
Bill Brown <bill.a.brown90@gmail.com>
2023-03-13 19:01:28 -0700

    Basics of using Props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // make a function for if statement for <p>
+  const isBio = () => {
+    if (props.bio === ""||props.bio===undefined){
+    return null
+  }else {
+    return <p>{props.bio}</p> 
+  }
+}
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+  
+      <Links github={props.gitHub} linkedin={props.linkedIn}/>
+   
     </div>
   );
 }
